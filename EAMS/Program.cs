815599ffa3368cdec9bfddd225057740cc4b4b8f
Minesh Ahirekar <minesh123.ma@gmail.com>
2023-12-25 17:09:56 +0530

    commit

--- EAMS/Program.cs
+++ EAMS/Program.cs
@@ -25,10 +25,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("../swagger/v1/swagger.json", "My API V1");
-    });
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
