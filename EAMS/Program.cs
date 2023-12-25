dc9b9a6d8a1427d160b7ca3678cc15a29e26bfdc
Minesh Ahirekar <minesh123.ma@gmail.com>
2023-12-25 08:28:45 +0530

    resolve cors

--- EAMS/Program.cs
+++ EAMS/Program.cs
@@ -3,13 +3,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddCors();
+builder.Services.AddCors(option =>
+    option.AddDefaultPolicy(builder =>
+        builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()
+    )
+    );
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseCors();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
